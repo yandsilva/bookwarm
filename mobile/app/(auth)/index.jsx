d76a64e3533a043d18bfb930561ef69d28676b5c
YAN SILVA LEAL <yan.sleal@senacsp.edu.br>
2025-07-17 16:27:11 -0300

    feat: corrigido a conexão com o backend, criação de login e login

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -8,19 +8,29 @@ import {
   ActivityIndicator,
   KeyboardAvoidingView,
   Platform,
+  Alert,
 } from "react-native";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "../../assets/styles/login.styles";
 import COLORS from "../../constants/colors";
+import { useAuthStore } from "../../store/authStore";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const { isCheckingAuth, isLoading, login } = useAuthStore();
 
-  const handleLogin = () => {};
+  const handleLogin = async () => {
+    const result = await login(email, password);
+
+    if (!result.success) {
+      Alert.alert("Error", result.error);
+    }
+  };
+
+  if (isCheckingAuth) return null;
 
   return (
     <KeyboardAvoidingView
--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -9,13 +9,49 @@ export const useAuthStore = create((set) => ({
   register: async (username, email, password) => {
     set({ isLoading: true });
     try {
-      const response = await fetch("http://localhost:3000/api/auth/register", {
+      const response = await fetch(
+        "http://10.152.46.57:3000/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            email,
+            password,
+          }),
+        }
+      );
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Something went wrong");
+      }
+
+      await AsyncStorage.setItem("user", JSON.stringify(data.user));
+      await AsyncStorage.setItem("token", data.token);
+
+      set({ token: data.token, user: data.user, isLoading: false });
+
+      return { success: true, message: "User registered successfully" };
+    } catch (error) {
+      set({ isLoading: false });
+      return { success: false, message: error.message };
+    }
+  },
+
+  login: async (email, password) => {
+    console.log(email, password);
+    set({ isLoading: true });
+    try {
+      const response = await fetch("http://10.152.46.57:3000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          username,
           email,
           password,
         }),
@@ -37,5 +73,25 @@ export const useAuthStore = create((set) => ({
       set({ isLoading: false });
       return { success: false, message: error.message };
     }
+  },
+
+  checkAuth: async () => {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      const userJson = await AsyncStorage.getItem("user");
+      const user = userJson ? JSON.parse(userJson) : null;
+
+      set({ token, user });
+    } catch (error) {
+      console.log("Auth check failed", error);
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  logout: async () => {
+    await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("user");
+    set({ token: null, user: null });
   },
 }));
